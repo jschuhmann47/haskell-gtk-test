52f2c9373ab204b49d7364d52fd7fcabe9f87660
jschuhmann47 <jschuhmann@frba.utn.edu.ar>
2023-06-01 14:10:47 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Haskell GTK test
+
+Ejemplo extraído de [este repo](https://github.com/owickstrom/gi-gtk-declarative/blob/master/examples)
---
+++ app/Main.hs
@@ -0,0 +1,51 @@
+{-# LANGUAGE OverloadedLabels  #-}
+{-# LANGUAGE OverloadedLists   #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Hello where
+
+import           Control.Monad                  ( void )
+import           Data.Function                  ( (&) )
+import           Data.Text                      ( Text )
+import           GI.Gtk                         ( Label(..)
+                                                , Window(..)
+                                                )
+import           GI.Gtk.Declarative
+
+import           GI.Gtk.Declarative.App.Simple
+import           Pipes
+import qualified Pipes.Extras                  as Pipes
+
+data State = Initial | Greeting Text
+
+data Event = Greet Text | Closed
+
+view' :: State -> AppView Window Event
+view' s =
+  bin
+      Window
+      [ #title := "Hello"
+      , on #deleteEvent (const (True, Closed))
+      , #widthRequest := 400
+      , #heightRequest := 300
+      ]
+    $ case s of
+        Initial      -> widget Label [#label := "Nothing here yet."]
+        Greeting who -> widget Label [#label := who]
+
+update' :: State -> Event -> Transition State Event
+update' _ (Greet who) = Transition (Greeting who) (return Nothing)
+update' _ Closed      = Exit
+
+main :: IO ()
+main = void $ run App { view         = view'
+                      , update       = update'
+                      , inputs       = [greetings]
+                      , initialState = Initial
+                      }
+ where
+  greetings =
+    cycle ["Joe", "Mike"]
+      & map (\n -> Greet ("Hello, " <> n))
+      & Pipes.each
+      & (>-> Pipes.delay 1.0)
